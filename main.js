67ab77ad16d03a6a391dd224fef6219640352533
atskoj <chastonmax@gmail.com>
2021-07-29 16:23:12 +0100

    Added bubble sort

---
+++ main.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+    var canvas = document.getElementById("sortingCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var sortingArray = [];
+
+    function initLines()
+    {
+        for (var i=0; i<500; i++)
+        {
+            sortingArray[i] = Math.floor(((Math.random())*100))
+        }
+    }
+
+    function drawLines(arr)
+    {
+        for (var i=0; i<arr.length; i++)
+        {
+            ctx.beginPath(); 
+            ctx.moveTo((i+1)*2+0.5, 0);
+            ctx.lineTo((i+1)*2+0.5, arr[i]);
+            ctx.stroke();
+            ctx.moveTo(0,0);
+        }
+    }
+
+    function bubbleSort(arr)
+    {
+        for (var i=0; i<arr.length-1; i++)
+        {
+            if (arr[i]>arr[i+1])
+            {
+                var temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+            //console.log(arr);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawLines(arr);
+        }
+    }
+
+    initLines();
+    drawLines(sortingArray);
+    var intervalID = setInterval(bubbleSort, 1, sortingArray);
+}
